35f104361792f1eecfb337916d0bbee6e0d5d36e
Jack D. Maher <oatsforsale@gmail.com>
2026-01-16 17:58:14 +0000

    Flyin

--- Group 1 Spectrum Code/SpectrumGenerator.py
+++ Group 1 Spectrum Code/SpectrumGenerator.py
@@ -14,22 +14,23 @@ with open("Group 1 Spectrum Code/PlanetaryParameters.csv", newline="") as Planet
 		data.append(conv)
 planetaryParameters = np.array(data, dtype=float)
 
-i=0
-while i < len(planetaryParameters):
-	Rp = planetaryParameters[i][0]  # Planet radius in units of Earth radii
-	Mp = planetaryParameters[i][1]  # Planet mass in units of Earth masses
-	Tp = planetaryParameters[i][2]  # Planet temperature in K
-	mu = planetaryParameters[i][3]  # Mean molecular weight in atomic mass units
-	Pcloud = planetaryParameters[i][4]  # Pressure at top of cloud deck in bar
-	Pref = planetaryParameters[i][5]   # Reference pressure in bar
-	Rs = planetaryParameters[i][6]  # Stellar radius in units of Solar radii        
+rowCount=0
+while rowCount < len(planetaryParameters):
+	Rp = planetaryParameters[rowCount][0]  # Planet radius in units of Earth radii
+	Mp = planetaryParameters[rowCount][1]  # Planet mass in units of Earth masses
+	Tp = planetaryParameters[rowCount][2]  # Planet temperature in K
+	mu = planetaryParameters[rowCount][3]  # Mean molecular weight in atomic mass units
+	Pcloud = planetaryParameters[rowCount][4]  # Pressure at top of cloud deck in bar
+	Pref = planetaryParameters[rowCount][5]   # Reference pressure in bar
+	Rs = planetaryParameters[rowCount][6]  # Stellar radius in units of Solar radii        
 	Rp *= const.R_earth.value   # Convert Rp from units of R_Earth to m
 	Rs *= const.R_sun.value     # Convert Rs from units of R_Sun to m
 	Pcloud *= 1.0e5             # Convert Pcloud from bar to Pa
 	Pref *= 1.0e5               # Convert Pref from bar to Pa
 	mu *= sc.u                  # Convert mu from atomic mass units to kg
 	print(f"Processing planet with {header[0]}={Rp}, {header[1]}={Mp}, {header[2]}={Tp}, {header[3]}={mu}, {header[4]}={Pcloud}, {header[5]}={Pref}, {header[6]}={Rs}")
-	i=i+1
+	print(rowCount,len(planetaryParameters))
+	rowCount=rowCount+1
 
 	P = np.logspace(2.0,-5,100) * 1.0e5
 	T = Tp*np.ones_like(P)
@@ -43,12 +44,12 @@ while i < len(planetaryParameters):
 	g[i_Rp] = gp
 
 	for i in range(i_Rp + 1, len(P)):
-	g[i] = g[i_Rp] * r[i_Rp] * r[i_Rp] / (r[i-1] * r[i-1])
-	r[i] = r[i-1] - ( sc.k * 0.5 * (T[i-1]+T[i]) / (mu * g[i]) ) * np.log(P[i]/P[i-1]) 
+		g[i] = g[i_Rp] * r[i_Rp] * r[i_Rp] / (r[i-1] * r[i-1])
+		r[i] = r[i-1] - ( sc.k * 0.5 * (T[i-1]+T[i]) / (mu * g[i]) ) * np.log(P[i]/P[i-1]) 
 
 	for i in range(i_Rp-1, -1, -1):
-	g[i] = g[i_Rp] * r[i_Rp] * r[i_Rp] / (r[i+1] * r[i+1])
-	r[i] = r[i+1] - ( sc.k * 0.5 * (T[i+1]+T[i]) / (mu * g[i]) ) * np.log(P[i]/P[i+1])
+		g[i] = g[i_Rp] * r[i_Rp] * r[i_Rp] / (r[i+1] * r[i+1])
+		r[i] = r[i+1] - ( sc.k * 0.5 * (T[i+1]+T[i]) / (mu * g[i]) ) * np.log(P[i]/P[i+1])
 	# First, set up a dictionary which will contain all the log mixing ratios, and input the abundances of all molecules except H2 and He
 	logX = dict()
 	logX['h2o'] = -3.0
@@ -79,6 +80,86 @@ while i < len(planetaryParameters):
 	# With all the mixing ratios defined, we can calculate the mean molecular weight of the atmosphere, mu:
 	mu = 0.0
 	for mol in logX.keys():
-	mu += np.power(10.0,logX[mol])*mmw[mol]
+		mu += np.power(10.0,logX[mol])*mmw[mol]
 
 	print("The mean molecular weight is:", mu)
+
+
+
+	xsec_h2o = np.load('GivenResources/cross_section_files/Cross_section_files/h2o_xsec.npy')
+	lam_h2o = np.load('GivenResources/cross_section_files/Cross_section_files/h2o_lam.npy')*1e6 # convert to microns
+	P_h2o = np.power(10.0,np.load('GivenResources/cross_section_files/Cross_section_files/h2o_P.npy')) # already in Pa
+	T_h2o = np.load('GivenResources/cross_section_files/Cross_section_files/h2o_T.npy')
+
+	# H2-H2 and He-H2 molecule pairs cause absorption through a process called "collision-induced absorption". This data is wavelength- and temperature-dependent, but not pressure-dependent.
+	xsec_h2h2 = np.load('GivenResources/cross_section_files/Cross_section_files/h2_h2_xsec.npy')
+	lam_h2h2 = np.load('GivenResources/cross_section_files/Cross_section_files/h2_h2_lam.npy')
+
+	xsec_heh2 = np.load('GivenResources/cross_section_files/Cross_section_files/he_h2_xsec.npy')
+	lam_heh2 = np.load('GivenResources/cross_section_files/Cross_section_files/he_h2_lam.npy')
+
+	lam = np.linspace(0.61,5.0,200)
+
+	log_xsec_dict = dict()
+	interp_xsec = RegularGridInterpolator((lam_h2o,P_h2o,T_h2o),xsec_h2o,'linear')
+	lamlam, PP = np.meshgrid(lam,P,indexing="ij")
+
+	log_xsec_dict['h2o'] = interp_xsec((lamlam,PP,T[0])) # this assumes that the atmosphere is isothermal
+
+	log_cia_dict = dict()
+	log_cia_dict['h2h2'] = np.interp(lam, lam_h2h2, xsec_h2h2)
+	log_cia_dict['heh2'] = np.interp(lam, lam_heh2, xsec_heh2)
+
+	sum_nsigma = np.zeros((len(lam), len(P)))
+	for mol in log_xsec_dict.keys():
+		sum_nsigma += n[np.newaxis,:]*np.power(10.0,logX[mol])*np.power(10.0,log_xsec_dict[mol])
+
+	sum_nsigma += n[np.newaxis,:]*n[np.newaxis,:]*np.power(10.0,logX['h2'])*np.power(10.0,logX['h2'])*np.power(10.0,log_cia_dict['h2h2'])[:,np.newaxis]
+	sum_nsigma += n[np.newaxis,:]*n[np.newaxis,:]*np.power(10.0,logX['he'])*np.power(10.0,logX['h2'])*np.power(10.0,log_cia_dict['heh2'])[:,np.newaxis]
+
+	integral_gt_Rp = np.zeros((len(lam))) # initialise an array where we'll store the "r>Rp" integral
+	integral_lt_Rp = np.zeros((len(lam))) # initialise an array where we'll store the "r<Rp" integral
+
+	exptau = np.zeros((len(P),len(lam)))
+	transit_depth = np.zeros((len(lam)))
+
+	# Compute the total optical depth at each impact parameter and wavelength #
+
+	opacity = sum_nsigma #(kappa+sigma)
+	# For each layer of atmosphere
+
+	for i in range(len(r)-1):
+
+		s_tot = np.sqrt(r[i:]*r[i:]-r[i]*r[i])
+		ds = s_tot[1:]-s_tot[:-1]
+
+		tau_tot = np.sum((opacity[:, i:-1] + opacity[:, i+1:])*ds[np.newaxis,:],axis=-1)
+
+		# If r[i] is deeper that the top of the cloud, the atmosphere here is fully opaque and tau is very large.
+		if P[i] > Pcloud:
+			tau_tot += 1000.0
+
+		# We need e^(-tau) for the integral, so let's calculate that here. Adding 1.0-e-250 avoids errors when taking the log of this in cases where e^-tau is essentially zero.
+		exptau[i, :] = np.exp(-1.0*tau_tot) + 1.0e-250
+
+
+	# Compute transit depth terms. Note that we take an average of the i^th and (i+1)^th terms in the integrals.
+	for i in range(len(r)-1):
+
+		# Rays travelling through atmosphere above Rp
+		if (r[i] >= Rp):
+			integral_gt_Rp[:] += 0.5*((r[i]*(1.0 - exptau[i, :]) + (r[i+1]*(1.0 - exptau[i+1, :])))*(r[i+1] - r[i]))
+
+		# Rays travelling through atmosphere below Rp
+		if (r[i] < Rp):
+			integral_lt_Rp[:] += 0.5*((r[i]*(exptau[i, :]) + (r[i+1]*(exptau[i+1, :])))*(r[i+1] - r[i]))
+
+	# Compute effective transit depth (transmission spectrum) #
+	transit_depth[:] = (Rp*Rp + 2.0*integral_gt_Rp[:] - 2.0*integral_lt_Rp[:])/(Rs*Rs)
+
+
+	plt.plot(lam,transit_depth*1e6) #convert transit_depth into units of ppm
+	plt.xlabel('Wavelength (microns)')
+	plt.ylabel('Transit Depth (ppm)')
+	#plt.xlim([1.1,1.7])
+	plt.savefig(f'TransmissionSpectrum{rowCount}.png')
