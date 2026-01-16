07f29e3d181883324d5a931f6950b279343f7290
Jack D. Maher <oatsforsale@gmail.com>
2026-01-16 17:23:02 +0000

    reccursive processing

--- Group 1 Spectrum Code/SpectrumGenerator.py
+++ Group 1 Spectrum Code/SpectrumGenerator.py
@@ -14,6 +14,19 @@ with open("Group 1 Spectrum Code/PlanetaryParameters.csv", newline="") as Planet
 		data.append(conv)
 planetaryParameters = np.array(data, dtype=float)
 
-print("Header:", header)
-print("Planetary parameters array:\n", planetaryParameters)
-
+i=0
+while i < len(planetaryParameters):
+	Rp = planetaryParameters[i][0]  # Planet radius in units of Earth radii
+	Mp = planetaryParameters[i][1]  # Planet mass in units of Earth masses
+	Tp = planetaryParameters[i][2]  # Planet temperature in K
+	mu = planetaryParameters[i][3]  # Mean molecular weight in atomic mass units
+	Pcloud = planetaryParameters[i][4]  # Pressure at top of cloud deck in bar
+	Pref = planetaryParameters[i][5]   # Reference pressure in bar
+	Rs = planetaryParameters[i][6]  # Stellar radius in units of Solar radii        
+	Rp *= const.R_earth.value   # Convert Rp from units of R_Earth to m
+	Rs *= const.R_sun.value     # Convert Rs from units of R_Sun to m
+	Pcloud *= 1.0e5             # Convert Pcloud from bar to Pa
+	Pref *= 1.0e5               # Convert Pref from bar to Pa
+	mu *= sc.u                  # Convert mu from atomic mass units to kg
+	print(f"Processing planet with {header[0]}={Rp}, {header[1]}={Mp}, {header[2]}={Tp}, {header[3]}={mu}, {header[4]}={Pcloud}, {header[5]}={Pref}, {header[6]}={Rs}")
+	i=i+1
