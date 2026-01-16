459a15be38ed0bf8f09f14e289341d8bf6b79f4b
Jack D. Maher <oatsforsale@gmail.com>
2026-01-16 16:32:15 +0000

    parameters

--- Group 1 Spectrum Code/SpectrumGenerator.py
+++ Group 1 Spectrum Code/SpectrumGenerator.py
@@ -1,1 +1,11 @@
-import os
+import numpy as np
+import scipy.constants as sc
+from scipy.interpolate import RegularGridInterpolator
+import astropy.constants as const
+import matplotlib.pyplot as plt
+import csv
+
+with open('Group 1 Spectrum Code/PlanetaryParameters.csv', mode='r') as file:
+    reader = csv.reader(file)
+    parameters = {rows[0]: float(rows[1]) for rows in reader}
+print(parameters[0])
