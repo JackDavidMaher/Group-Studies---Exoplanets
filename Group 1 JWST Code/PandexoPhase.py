0effbbb59a480c50db287bccfc6698e2d50003f6
evelynmccag <EXM216@student.bham.ac.uk>
2026-01-27 15:53:35 +0000

    started on PandExo csv

--- Group 1 JWST Code/PandexoPhase.py
+++ Group 1 JWST Code/PandexoPhase.py
@@ -1,74 +1,41 @@
 # use Group 1 Spectrum Code/SpectrumGenerator.py to read the csv file PandExoParametrers and update the parameters below accordingly
 
-
-
-import warnings
-warnings.filterwarnings('ignore')
-import pandexo.engine.justdoit as jdi # THIS IS THE HOLY GRAIL OF PANDEXO
-import numpy as np
+import csv
 import os
-exo_dict = jdi.load_exo_dict()
-exo_dict['observation']['sat_level'] = 80    #saturation level in percent of full well 
-exo_dict['observation']['sat_unit'] = '%' 
-exo_dict['observation']['noccultations'] = 2 #number of transits 
-exo_dict['observation']['R'] = None          #fixed binning. I usually suggest ZERO binning.. you can always bin later 
-                                             #without having to redo the calcualtion
-exo_dict['observation']['baseline'] = 1.0    #fraction of time in transit versus out = in/out
-exo_dict['observation']['baseline_unit'] = 'frac' 
-exo_dict['observation']['noise_floor'] = 0   #this can be a fixed level or it can be a filepath 
-exo_dict['star']['type'] = 'phoenix'        #phoenix or user (if you have your own)
-exo_dict['star']['mag'] = 8.0               #magnitude of the system
-exo_dict['star']['ref_wave'] = 1.25         #For J mag = 1.25, H = 1.6, K =2.22.. etc (all in micron)
-exo_dict['star']['temp'] = 5500             #in K 
-exo_dict['star']['metal'] = 0.0             # as log Fe/H
-exo_dict['star']['logg'] = 4.0
-exo_dict['star']['radius'] = 1
-exo_dict['star']['r_unit'] = 'R_sun'    
-exo_dict['planet']['type'] = 'constant'
-exo_dict['planet']['radius'] = 1                      #other options include "um","nm" ,"Angs", "secs" (for phase curves)
-exo_dict['planet']['r_unit'] = 'R_jup'  
-exo_dict['planet']['transit_duration'] = 2.0*60.0*60.0 
-exo_dict['planet']['td_unit'] = 's'
-exo_dict['planet']['f_unit'] = 'rp^2/r*^2'
-print('Starting TEST run')
-jdi.run_pandexo(exo_dict, ['NIRSpec G140H'], save_file=False)
-print('SUCCESS') 
-import pandexo.engine.justplotit as jpi 
-import pickle as pk
+
+# This section reads the PandExoParameters.csv file and extracts planetary parameters
+with open("Group 1 Spectrum Code/PandExoParameters.csv", newline="") as PandExoParametersFile:
+	reader = csv.reader(PandExoParametersFile)
+	header = next(reader)
+	data = []
+	planetNames = []
+	for row in reader:
+		row = [item.strip() for item in row]
+		if len(row) == 0:
+			continue
+		*nums, name = row
+		conv = [float(item) if item != "" else np.nan for item in nums]
+		data.append(conv)
+		planetNames.append(name)
+	planetaryParameters = np.array(data, dtype=float)
+
+rowCount=0
+while rowCount < len(planetaryParameters):
+	Rp = planetaryParameters[rowCount][0]  # Planet radius in units of Earth radii
+	Mp = planetaryParameters[rowCount][1]  # Planet mass in units of Earth masses
+	Tp = planetaryParameters[rowCount][2]  # Planet temperature in K
+	mu = planetaryParameters[rowCount][3]  # Mean molecular weight in atomic mass units
+	Pcloud = planetaryParameters[rowCount][4]  # Pressure at top of cloud deck in bar
+	Pref = planetaryParameters[rowCount][5]   # Reference pressure in bar
+	Rs = planetaryParameters[rowCount][6]  # Stellar radius in units of Solar radii     
+	PName = planetNames[rowCount]  # Planet name (string)   
+	Rp *= const.R_earth.value   # Convert Rp from units of R_Earth to m
+	Rs *= const.R_sun.value     # Convert Rs from units of R_Sun to m
+	Pcloud *= 1.0e5             # Convert Pcloud from bar to Pa
+	Pref *= 1.0e5               # Convert Pref from bar to Pa
+	mu *= sc.u                  # Convert mu from atomic mass units to kg
+	#print(f"Processing planet with {header[0]}={Rp}, {header[1]}={Mp}, {header[2]}={Tp}, {header[3]}={mu}, {header[4]}={Pcloud}, {header[5]}={Pref}, {header[6]}={Rs}")
+	#print(rowCount,len(planetaryParameters))
+	rowCount=rowCount+1
 
 
-exo_dict = jdi.load_exo_dict()
-
-exo_dict['star']['type'] = 'phoenix'      
-exo_dict['star']['temp'] = 3101 
-exo_dict['star']['metal'] =   0.24             ## as log Fe/H
-exo_dict['star']['logg'] =  5.0286            ## log gravity cgs
-exo_dict['star']['mag'] = 9.75
-exo_dict['star']['ref_wave'] = 1.25        #For J mag = 1.25, H = 1.6, K =2.22.. etc (all in micron) 
-exo_dict['star']['radius'] = 0.2162
-exo_dict['star']['r_unit'] = 'R_sun'
-exo_dict['planet']['radius'] = 0.24382235
-exo_dict['planet']['r_unit'] = 'R_jup'     ## or R_earth
-
-exo_dict['planet']['transit_duration'] = 0.036235833333333335
-exo_dict['planet']['td_unit'] = 'd'
-
-exo_dict['planet']['type'] = 'constant'  ## 'constant', 'user'
-##exo_dict['planet']['exopath'] = 'wasp12b.txt'
-exo_dict['planet']['f_unit'] = 'rp^2/r*^2'
-#exo_dict['planet']['w_unit'] = 'um'. # wavelength unit for user defined spectra
-
-exo_dict['observation']['baseline'] = 1.0 
-exo_dict['observation']['baseline_unit'] = 'frac'
-exo_dict['observation']['noccultations'] = 1         ## number of transits
-
-inst_dict = jdi.load_mode_dict('NIRSpec G235H')        ## Choose instrument/mode
-#inst_dict["configuration"]["detector"]["subarray"] = 'substrip256'  ##to change the sub array
-exo_dict['observation']['sat_level'] = 100    #saturation level in percent of full well 
-exo_dict['observation']['sat_unit'] = '%' 
-exo_dict['observation']['noise_floor'] = 0
-
-result = jdi.run_pandexo(exo_dict, inst_dict, save_file=False) ## save file = 'test.p'
-
-x,y, e = jpi.jwst_1d_spec(result, R=100, num_tran=1, model=False, x_range=[1.6,3.0])
-data = jpi.jwst_2d_det(result)
